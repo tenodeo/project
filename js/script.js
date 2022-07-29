9da792c9920222bccaad2fafa9cb957f65b92e0c
Temirlan <tesovin9@gmail.com>
2022-07-30 03:47:41 +0600

    first practice

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-const category = "toys";
-console.log(`https//${category}`); 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?",""),
+      b = prompt("Насколько вы его оцените?",""),
+      c = prompt("Один из последних просмотренных фильмов?",""),
+      d = prompt("Насколько вы его оцените?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
