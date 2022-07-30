7baa1ee3a4f9b28c1cdf2fe20334579039a96b83
Temirlan <tesovin9@gmail.com>
2022-07-30 22:50:48 +0600

    callback function

--- js/script.js
+++ js/script.js
@@ -29,3 +29,13 @@ function showDB(hidden){
     if(hidden)
     console.log("Bye");
 }
+
+function done(){
+    console.log("Я уже прошел этот курс!");
+}
+function learnJS(lang, callback){
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+learnJS("JavaScript",done);
