271aaf56db6a4b88a5308687596080540b0777d5
Temirlan <tesovin9@gmail.com>
2022-07-30 23:47:21 +0600

    методы в обьектах, деструктуризация, вложенность объектов,цикл for in

--- js/script.js
+++ js/script.js
@@ -39,3 +39,32 @@ function learnJS(lang, callback){
 }
 
 learnJS("JavaScript",done);
+
+
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    },
+    makeTest: function(){
+        console.log("Hello World!");
+    }
+};
+console.log(Object.keys(options).length);
+options.makeTest();
+
+const {border,bg} = options.colors;
+console.log(border.+" "+bg);
+
+for(let key in options){
+    if(typeof(options[key] === "object")){
+        for(let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    }
+    else 
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+}
